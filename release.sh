38a3421c2502a082a12dc6caddae3ade93dd6961
Chintan Karia <chintankaria@Chintans-MacBook-Pro-2.local>
2018-03-22 16:45:43 -0600

    updated

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ git checkout -b $releaseBranch $masterBranch
 # create the release branch from the -develop branch
 git push --set-upstream origin $releaseBranch 
 
-echo -e "Are you sure you want to merge the previous release branch $previousReleaseBranch into new $releaseBranch? (y|n): \c "
+echo -e "Do you have a previous release branch to merge into the new one? \c "
 read confirm
 
 if([ $confirm == "y" ]) then
@@ -30,27 +30,33 @@ if([ $confirm == "y" ]) then
     echo -e "Enter the previous release branch number e.g. (release.0.0.50) \c "
     read previousReleaseBranch
 
-    # checkout previous release branch
-    git checkout $previousReleaseBranch
-
-    # git pull origin previous releae branch
-    git pull origin $previousReleaseBranch
-
-    # checkout new release branch
-    git checkout $releaseBranch 
-
-	echo "Merging $previousReleaseBranch into $releaseBranch "
-
-	result=`git merge --no-ff $previousReleaseBranch`
-
-	if([ "$result" == "`echo $result | grep "^Already up-to-date\."`" ]) then
-        echo "The branch is already up to date.";
+    if[ -z "$previousReleaseBranch"]
+        echo "Created new local and remote release branch i.e. $releaseBranch \c"
         exit 1;
     else
-    	git push origin $releaseBranch
-    	echo "Finished merging."
-        exit 1;
+        # checkout previous release branch
+        git checkout $previousReleaseBranch
+
+        # git pull origin previous releae branch
+        git pull origin $previousReleaseBranch
+
+        # checkout new release branch
+        git checkout $releaseBranch 
+
+        echo "Merging $previousReleaseBranch into $releaseBranch "
+
+        result=`git merge --no-ff $previousReleaseBranch`
+
+        if([ "$result" == "`echo $result | grep "^Already up-to-date\."`" ]) then
+            echo "The branch is already up to date.";
+            exit 1;
+        else
+            git push origin $releaseBranch
+            echo "Finished merging."
+            exit 1;
+        fi
     fi
 else
-	exit 1;
-fi	
+    echo "Created new local and remote release branch i.e. $releaseBranch \c"
+    exit 1;
+fi  
